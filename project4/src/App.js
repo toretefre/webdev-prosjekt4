efafabf15c71a693e8d17a48afa13af03de2dd0a
Jonas Jevnaker Aas <jonasjaas@gmail.com>
2018-11-01 12:26:39 +0100

    Recreated the project to include decoration in MobX

---
+++ project4/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+//components
+import Emoji from './Emoji';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Tester:)
+      </div>
+    );
+  }
+}
+
+export default App;
