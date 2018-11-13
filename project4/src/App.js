8b23cb6dc04c39d5664f1cdfd347f5f860968dd3
Jonas Jevnaker Aas <jonasjaas@gmail.com>
2018-11-13 12:51:52 +0100

    Fixed infinitescroll and some gui changes. #14

--- project4/src/App.js
+++ project4/src/App.js
@@ -7,9 +7,22 @@ import MovieList from './components/MovieList.js';
 import './App.css';
 
 
+
 @inject('movieStore')
 @observer
 class App extends Component {
+
+   //ComponentDidMount loads 20 movies when the user enters the code
+   //Adding eventlistener for scrolling (which is being used in handlescroll)
+   componentDidMount() {
+        this.props.movieStore.fetchMovieData("");
+        document.addEventListener('scroll', this.handleScroll);
+   }
+
+   //ComponentWillUnmount is removing the eventlistener
+   componentWillUnmount() {
+        document.removeEventListener('scroll', this.handleScroll);
+   }
 
   //Function to change the view between grid and list.
   //The function sets the value of the observable variable "expandmovie" to the opposite value (true/false)
@@ -26,27 +39,30 @@ class App extends Component {
     }
   };
 
-  async fetchMovies(searchText){
-      await this.props.movieStore.fetchMovieData(searchText);
-      //this.showMovies(searchText);
-  }
-
+  //Gets the chosen genre from the the dropdown and passes this to the moviestore, which retrieves a new set of movies based on the genre
   setGenre(){
       let selectedGenre = document.getElementById("genrePicker").value;
       this.props.movieStore.setGenre(selectedGenre)
   }
 
-
-    handleRatingChange = (event, value) => {
-      console.log(value);
-        this.props.movieStore.setMinRating(value);
-    };
+  handleRatingChange = (event, value) => {
+    this.props.movieStore.setMinRating(value);
+  };
 
   //Function to fetch movies from API
   async fetchMovies(searchText){
       document.getElementById("infoText").innerHTML = "Showing results for '" + searchText + "'";
       await this.props.movieStore.fetchMovieData(searchText);
   }
+
+  //Detects whether the user is scrolled to the bottom or not
+  //If the user is scrolled to the bottom, it will increase the number of movie fetched.
+  handleScroll = () =>{
+      if((document.documentElement.scrollTop + document.documentElement.clientHeight + 1) >= document.documentElement.scrollHeight){
+          this.props.movieStore.increaseFetchedMovies();
+          this.props.movieStore.fetchMovieData(this.searchText);
+      }
+  };
 
 
   //-----RENDER----------
@@ -94,7 +110,7 @@ class App extends Component {
                 </div>
               </div>
             <div id="moviesContainer">
-                <MovieList changeView = {() => this.changeView()} minRating={this.props.movieStore.minRating} genre={this.props.movieStore.genre}}/>
+                <MovieList fetchMoreData = {() => this.fetchMovies()} changeView = {() => this.changeView()} minRating={this.props.movieStore.minRating} genre={this.props.movieStore.genre}/>
             </div>
         </React.Fragment>
     );
--- project4/src/stores/MovieStore.js
+++ project4/src/stores/MovieStore.js
@@ -4,23 +4,42 @@ import axios from "axios";
 
 //This is our store, where all the state is stored.
 class MovieStore extends Component {
+    @observable fetchedMovies = 0;
     @observable expandMovie = false;
     @observable movies = [];
+    @observable tempList = [];
+    @observable resList = [];
     @observable genre = "All";
     @observable minRating = 1;
+    @observable tempSearchString = "";
 
     //Function to retrieve data from our api.
     //It fetches data together a given searchparam from the user.
     //Axios will try to retrieve data, and then store the data in the "movies"-list
     @action async fetchMovieData(searchParam) {
-        const endpoint = 'http://it2810-32.idi.ntnu.no:8080/movies/'+searchParam;
+        if (!searchParam){
+            searchParam = "";
+        }
+        if (this.tempSearchString !== searchParam){
+            this.fetchedMovies = 0;
+            this.movies = [];
+            this.tempSearchString = searchParam;
+        }
+        let endpoint = 'http://it2810-32.idi.ntnu.no:8080/movies/'+ searchParam+ "?startindex="+this.fetchedMovies;
+        /*if(genreslece) {
+            endpoint += "&genre="+this.valggjasner;
+        }
+        if(thresholdnoe){
+            endpoint += "&threshold="+this.novalgt;
+        }*/
         await axios.get(endpoint)
             .then(res => {
                 if(res.data.error){
                     throw res.data.error;
                 }
-                this.movies = res.data;
+                this.movies = this.movies.slice().concat(res.data);
             });
+
     }
 
     @action setExpandMovie(val){
@@ -33,6 +52,10 @@ class MovieStore extends Component {
 
     @action setMinRating(val){
         this.minRating = val;
+    }
+
+    @action increaseFetchedMovies(){
+        this.fetchedMovies += 20;
     }
 }
 
